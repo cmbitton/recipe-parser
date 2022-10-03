9d71ec74c116b5e0b27b918a2a9ad5c27da24959
Curtis Bitton <cmbitton@gmail.com>
2022-10-03 13:28:15 -0400

    Add edit buttons and format CSS
    Added button and scripts to edit document
    and updated formatting for mobile, tablet
    and desktop layouts

--- script.js
+++ script.js
@@ -33,9 +33,10 @@ function writeInstructionList(instructionsArray) {
             instructionName.append(steps);
             for (let stepNum = 0; stepNum < instructionsArray[i].steps.length; stepNum++) {
                 //checks for keywords that indicate photos
-                if (instructionsArray[i].steps[stepNum].step.includes('Simply') || instructionsArray[i].steps[stepNum].step.includes('Dotdash') ){ }
+                if (instructionsArray[i].steps[stepNum].step.includes('Simply') || instructionsArray[i].steps[stepNum].step.includes('Dotdash')) { }
                 //check for Nutrition Facts being grouped into instructions
                 else if (instructionsArray[i].steps[stepNum].step.includes('Nutrition Facts')) { break }
+                //adds instruction step and undoes bolding
                 else {
                     const step = document.createElement('li');
                     steps.append(step);
@@ -61,7 +62,7 @@ function writeInstructions(instructions) {
 
 }
 // script to replace image with a searched image if image not found
-function replaceImage(search, image){
+function replaceImage(search, image) {
     const options = {
         method: 'GET',
         headers: {
@@ -89,58 +90,89 @@ run.addEventListener('click', () => {
         }
     };
     const input = document.querySelector('.recipe-input-url');
-    const recipeContainer = document.querySelector('.recipe-content-container');
-    recipeContainer.setAttribute('contenteditable', 'true')
     const title = document.querySelector('.title');
     const recipeImage = document.querySelector('.recipe-image');
     const readyIn = document.querySelector('.ready-in');
     const servings = document.querySelector('.servings');
     const infoMessage = document.querySelector('.ready-in-servings');
-    fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/extract?url=${input.value}`, options)
-        .then(response => response.json())
-        .then(response => {
-            console.log(response);
-            title.textContent = response.title;
-            if (response.readyInMinutes !== -1) {
-                readyIn.textContent = `Ready In: ${Math.floor(response.readyInMinutes / 60)} hours and ${response.readyInMinutes % 60} Minutes`;
-                infoMessage.style.columnGap = '40px';
-            }
-            if (response.servings !== -1) {
-                servings.textContent = `Servings: ${response.servings}`;
-            }
-            //checks if image is available, if not runs search api and uses that image
-            if (response.image !== null){
-            recipeImage.src = response.image;
-            }
-            else{
-                if(response.title === 'Search Page' || response.title === ''){
-                    replaceImage(response.sourceUrl.slice(36, response.sourceUrl.indexOf('&')), recipeImage);
-                    title.textContent = response.sourceUrl.slice(36, response.sourceUrl.indexOf('&'));
+    if (input.value !== '') {
+        fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/extract?url=${input.value}`, options)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+                title.textContent = response.title;
+                if (response.readyInMinutes !== -1) {
+                    readyIn.textContent = `Ready In: ${Math.floor(response.readyInMinutes / 60)} hours and ${response.readyInMinutes % 60} Minutes`;
+                    infoMessage.style.columnGap = '40px';
                 }
-                else{
-                    replaceImage(response.title, recipeImage);
+                if (response.servings !== -1) {
+                    servings.textContent = `Servings: ${response.servings}`;
+                }
+                //checks if image is available, if not runs search api and uses that image
+                if (response.image !== null) {
+                    recipeImage.src = response.image;
+                }
+                else {
+                    if (response.title === 'Search Page' || response.title === '') {
+                        replaceImage(response.sourceUrl.slice(36, response.sourceUrl.indexOf('&')), recipeImage);
+                        title.textContent = response.sourceUrl.slice(36, response.sourceUrl.indexOf('&'));
+                    }
+                    else {
+                        replaceImage(response.title, recipeImage);
+
+                    }
 
                 }
+                writeIngedientList(response.extendedIngredients);
+                if (response.analyzedInstructions.length > 0) {
+                    writeInstructionList(response.analyzedInstructions);
+                }
+                else {
+                    writeInstructions(response.instructions);
+                }
 
-            }
-            writeIngedientList(response.extendedIngredients);
-            if(response.analyzedInstructions.length > 0){
-            writeInstructionList(response.analyzedInstructions);}
-            else{
-                writeInstructions(response.instructions);
-            }
-        })
-        .catch(err => title.textContent = err);
+            })
+            .catch(() => {
+                //adds error message and removes all text if no recipe found
+                title.textContent = 'ERROR FINDING RECIPE';
+                recipeImage.src = '';
+                infoMessage.textContent = '';
+                const ingredientsHeader = document.querySelector('.ingredients-header');
+                ingredientsHeader.textContent = ''
+            });
+    }
 })
 
 
+function editDocument() {
+    const editButton = document.querySelector('.edit-doc');
+    editButton.addEventListener('click', () => {
+        //set up the hover on element and edit elements
+        const recipeContainer = document.querySelector('.recipe-content-container');
+        if (recipeContainer.getAttribute('id') !== 'hov') {
+            recipeContainer.setAttribute('contenteditable', 'true');
+            recipeContainer.setAttribute('id', 'hov');
+        }
+    })
+}
+function finishEditing() {
+    const finishEditingButton = document.querySelector('.finish-editing');
+    finishEditingButton.addEventListener('click', () => {
+        //undo hover element
+        const recipeContainer = document.querySelector('.recipe-content-container');
+        if (recipeContainer.getAttribute('id') === 'hov') {
+            recipeContainer.setAttribute('contenteditable', 'false');
+            recipeContainer.removeAttribute('id', 'hov');
+        }
+    })
+}
 
-
-
-function saveToPDF(){
+function saveToPDF() {
     const saveButton = document.querySelector('.save-pdf');
     saveButton.addEventListener('click', () => {
         window.print();
     })
 }
 saveToPDF();
+editDocument();
+finishEditing();
